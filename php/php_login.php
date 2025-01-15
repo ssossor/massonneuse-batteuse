7d58cda3fc8b13fa36e9e9c5059ca57890666f7c
Asthral <prv.accs.perso@gmail.com>
2025-01-15 14:14:03 +0000

    ajout commentaire + identification login, supp d'index.php, doit être changé de dossier

--- php/php_login.php
+++ php/php_login.php
@@ -1,7 +1,10 @@
 <?php
+# definition de base
+
 define('ROOT', 'massonneuse-batteuse');
 session_start();
 
+# reprise de quelques conditions classics avec inspiration d'ancien fichier
 if (isset($_POST['login'] && !empty($_POST['login']) && isset($_POST['password'] && !empty($_POST['password']) {
     $login = htmlspecialchars(filter_input(INPUT_POST, 'login'));
     $password = htmlspecialchars(filter_input(INPUT_POST, 'password'));
@@ -9,6 +12,11 @@ if (isset($_POST['login'] && !empty($_POST['login']) && isset($_POST['password']
     if (!$login) or (!$password) {
         header('Location:./login.php');
     }
+        # verification des identifiants
+        if (($password == "Onaimebeaucoup") && ($login == "LescoursdeSeb")) {
+	    echo "vous êtes bien connecté";
+	} else {
+	    echo "erreur de mot de passe"
 
 } else {
     header('Location:./login.php');
