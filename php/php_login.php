0688d767c7e18d1d1bc9dddbac59479425fe0ffc
Asthral <prv.accs.perso@gmail.com>
2025-01-15 14:34:42 +0000

    ajout de session error ou session validate + commentaire

--- php/php_login.php
+++ php/php_login.php
@@ -10,15 +10,19 @@ if (isset($_POST['login'] && !empty($_POST['login']) && isset($_POST['password']
     $password = htmlspecialchars(filter_input(INPUT_POST, 'password'));
 
     if (!$login) or (!$password) {
+        $_SESSION['error'] = "/!\ Login ou mot de passe vide /!\";
         header('Location:./login.php');
     }
         # verification des identifiants
         if (($password == "Onaimebeaucoup") && ($login == "LescoursdeSeb")) {
-	    echo "vous êtes bien connecté";
+	    $_SESSION['valide'] = "Vous êtes connecté";
 	} else {
-	    echo "erreur de mot de passe"
+	    $_SESSION['error'] = "/!\ Erreur de mot de passe /!\";
 
 } else {
+    $_SESSION['error'] = "/!\ Il y a des champs sont vides ou non saisie /!\";
     header('Location:./login.php');
 }
+
+# les $_SESSION['erreur'] et ['valide'] serviront au début des fichiers, si ils sont set ils seront echo avec le message.
 ?>
